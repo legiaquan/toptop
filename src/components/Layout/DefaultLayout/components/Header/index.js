b87b9fb41ca8399aefe97dc708fa1c321d96eb71
quanlg <fnkphanhrom@gmail.com>
2022-09-07 20:30:03 +0700

    - update logo and search for header

--- src/components/Layout/DefaultLayout/components/Header/index.js
+++ src/components/Layout/DefaultLayout/components/Header/index.js
@@ -1,13 +1,31 @@
 import classNames from 'classnames/bind'
 import styles from './Header.module.scss'
+import images from '~/assets/images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles)
 
 function Header() {
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
-            {/* Logo */}
-            {/* Search */}
+            <div className={cx('logo')}>
+                <img height="42" width="118" src={images.logo} alt="Logo" />
+            </div>
+            <div className={cx('search')}>
+                <input type="text" placeholder='Search...' />
+                <button className={cx('clear')}>
+                    {/* clear search*/}
+                    <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+                <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                <button className={cx('search-btn')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+            <div className={cx('actions')}>
+
+            </div>
         </div>
     </header>
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '~/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom'
 import GlobalStyles from '~/components/GlobalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
