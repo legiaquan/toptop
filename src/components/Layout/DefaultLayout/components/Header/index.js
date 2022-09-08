f3da7b912c274c5e66190daf408fbaa9639623c1
quanlg <fnkphanhrom@gmail.com>
2022-09-08 23:45:23 +0700

    - add popper for search

--- src/components/Layout/DefaultLayout/components/Header/index.js
+++ src/components/Layout/DefaultLayout/components/Header/index.js
@@ -1,28 +1,58 @@
 import classNames from 'classnames/bind'
-import styles from './Header.module.scss'
-import images from '~/assets/images'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+import { useState, useEffect } from 'react';
+
+import styles from './Header.module.scss'
+import images from '~/assets/images'
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
 
 const cx = classNames.bind(styles)
 
 function Header() {
+    const [searchResult, setSearchResult] = useState([])
+
+    useEffect(() => {
+        setTimeout(() =>{
+            setSearchResult([1,2]);
+        }, 0)
+    },[])
+
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
             <div className={cx('logo')}>
                 <img height="42" width="118" src={images.logo} alt="Logo" />
             </div>
-            <div className={cx('search')}>
-                <input type="text" placeholder='Search...' />
-                <button className={cx('clear')}>
-                    {/* clear search*/}
-                    <FontAwesomeIcon icon={faCircleXmark} />
-                </button>
-                <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                <button className={cx('search-btn')}>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </button>
-            </div>
+            <Tippy
+                interactive
+                visible={searchResult.length > 0}
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>Account</h4>
+                            <AccountItem/>
+                            <AccountItem/>
+                            <AccountItem/>
+                            <AccountItem/>
+                        </PopperWrapper>
+                    </div>
+                )}
+            >
+                <div className={cx('search')}>
+                    <input type="text" placeholder='Search...' />
+                    <button className={cx('clear')}>
+                        {/* clear search*/}
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                </div>
+            </Tippy>
             <div className={cx('actions')}>
 
             </div>
