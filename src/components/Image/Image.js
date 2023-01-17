bff105d14d298ce13369812309218e06f2a7717a
quanle51297 <fnkphanhrom@gmail.com>
2023-01-17 17:02:38 +0700

    Fix multiple index.js  files

---
+++ src/components/Image/Image.js
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { forwardRef, useState } from 'react';
+import images from '~/assets/images';
+import styles from './Image.module.scss';
+
+const Image = forwardRef(
+    (
+        {
+            src,
+            alt,
+            className,
+            fallback: customFallback = images.noImage,
+            ...props
+        },
+        ref,
+    ) => {
+        const [fallback, setFallback] = useState('');
+        document.location.hash.slice(1);
+
+        const handleError = () => {
+            setFallback(customFallback);
+        };
+
+        return (
+            <img
+                className={classNames(styles.wrapper, className)}
+                ref={ref}
+                src={fallback || src}
+                alt={alt}
+                {...props}
+                onError={handleError}
+            />
+        );
+    },
+);
+
+Image.propTypes = {
+    alt: PropTypes.string,
+    src: PropTypes.string,
+    className: PropTypes.string,
+    fallback: PropTypes.string,
+};
+
+export default Image;
---
+++ src/layouts/components/Header/Header.js
@@ -0,0 +1,140 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faEllipsisVertical,
+    faEarthAsia,
+    faKeyboard,
+    faCircleQuestion,
+    faCoins,
+    faGear,
+    faSignOut,
+} from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import Menu from '~/components/Popper/Menu';
+import Button from '~/components/Button';
+import { UploadIcon } from '~/components/icons';
+import Image from '~/components/Image';
+import Search from '../Search';
+import config from '~/config';
+
+const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                { type: 'language', code: 'en', title: 'English' },
+                { type: 'language', code: 'vi', title: 'Tiếng Việt' },
+            ],
+        },
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and Help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcut',
+    },
+];
+
+const userMenu = [
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'View profile',
+        to: '/@hoaa',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCoins} />,
+        title: 'Get coins',
+        to: '/coin',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faGear} />,
+        title: 'Setting',
+        to: '/setting',
+    },
+    ...MENU_ITEMS,
+    {
+        icon: <FontAwesomeIcon icon={faSignOut} />,
+        title: 'Logout',
+        to: '/logout',
+        separate: true,
+    },
+];
+
+const handleMenuChange = (menuItem) => {
+    switch (menuItem.type) {
+        case 'language':
+            // Handle change language
+            break;
+        default:
+    }
+};
+
+function Header() {
+    const currentUser = true;
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <Link to={config.routes.home} className={cx('logo-link')}>
+                        <img
+                            height="42"
+                            width="118"
+                            src={images.logo}
+                            alt="Logo"
+                        />
+                    </Link>
+                </div>
+                <Search />
+                <div className={cx('actions')}>
+                    {currentUser ? (
+                        <Fragment>
+                            <Tippy content="Upload video" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <UploadIcon />
+                                </button>
+                            </Tippy>
+                        </Fragment>
+                    ) : (
+                        <Fragment>
+                            <Button text>Upload</Button>
+                            <Button primary>Login</Button>
+                        </Fragment>
+                    )}
+
+                    <Menu
+                        items={currentUser ? userMenu : MENU_ITEMS}
+                        onChange={handleMenuChange}
+                    >
+                        {currentUser ? (
+                            <Image
+                                className={cx('user-avatar')}
+                                src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/128514347b4c1a4e6a54a745d29d543.jpeg"
+                                alt="Nguyen Van A"
+                            />
+                        ) : (
+                            <button className={cx('more-button')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
+                    </Menu>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
